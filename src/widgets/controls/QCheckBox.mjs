cdb24b841b59a44d9efeae3cfcf76a88677b52f6
nori-pixl <sato.shigure4@gmail.com>
2026-07-19 12:39:48 +0900

    Create QCheckBox.mjs

---
+++ src/widgets/controls/QCheckBox.mjs
@@ -0,0 +1,10 @@
+import { QtWidget } from '../QtWidget.mjs';
+export class QCheckBox extends QtWidget {
+  constructor(id, props = {}) {
+    super(id, props);
+    this.props.text = props.text || "";
+    this.props.checked = !!props.checked;
+    if (props.onChange && typeof props.onChange !== 'function') throw new Error("QTxmljs: onChange must be a function");
+    this.onChange = props.onChange || null;
+  }
+}
